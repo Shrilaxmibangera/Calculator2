110c915587d50f28c18dfcc343b7870226b787bc
Shrilaxmi S Bangera <shrilaxmibangera@gmail.com>
2022-02-15 14:03:20 +0530

    next changes

--- caljs.js
+++ caljs.js
@@ -1,17 +1,18 @@
 var ans='';
 var res='';
 var answer='';
-function cal(btnvalue)
-{
+function cal(btnvalue) {
     ans=btnvalue;
-    if(ans=='=')
-    {
+    if(ans=='=') {
         res=eval(answer);
         console.log('Answer',res);
         document.getElementById('answer').value=res;
     }
-    else
-    {
+    else if(ans=='<') {
+        answer=answer.substr(0,answer.length-1);
+        document.getElementById('answer').value=answer;
+    }
+    else {
         answer+=ans;
         document.getElementById('answer').value=answer;
         console.log('ans',answer);
@@ -19,41 +20,24 @@ function cal(btnvalue)
     
     }
 }
-function clr()
-{
+function clr() {
         ans='';
         res='';
         answer='';
         document.getElementById('answer').value='';
-    
 }
-function FormingTable()
-{
+function FormingTable() {
     document.write("<link rel=\"stylesheet\" href=\"cal.css\">");
+    var arr=[1,2,3,'*',4,5,6,'-',7,8,9,'/','.','<','+','='];
     document.write('<table>');
     document.write('<tr><th colspan=3><input name="input" size="26" id="answer"></th><th><button id="clear" value="C" onclick="clr()">C</button></th></tr>');
-    for(let i=1;i<=9;i++)
-    {
-        if(i%3==1)
-        {
+    for(let i=1;i<=arr.length;i++) {
+        if(i%4==1) {
             document.write('<tr>');
         }
-          document.write('<td><button value="' , i , '" onclick=cal(this.value)>',i,'</button></td>');
-        if(i%3==0)
-        {
-            if (i==3) {
-                document.write('<td><button class="operator" value=+ onclick=cal(this.value)>+</button></td></tr>');
-            }
-            if (i==6) {
-                document.write('<td><button value=- onclick=cal(this.value)>-</button></td>');
-            }
-            if (i==9) {
-                document.write('<td><button value=* onclick=cal(this.value)>*</button></td></tr>');
-                document.write('<tr><td><button value=/ onclick=cal(this.value)>/</button></td>');
-                document.write('<td><button value=0 onclick=cal(this.value)>0</button></td>');
-                document.write('<td><button value=. onclick=cal(this.value)>.</button></td>');
-                document.write('<td><button value== onclick=cal(this.value)>=</button></td></tr>');
-            }   
+          document.write('<td><button value="' , arr[i-1] , '" onclick=cal(this.value)>',arr[i-1],'</button></td>');    
+        if(i%4==0) {
+            document.write('</tr>');
         }
     }
 }
