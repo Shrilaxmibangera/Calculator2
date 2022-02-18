17ca896d4544e6ad3d6b86450147af3cf698f4e9
Shrilaxmi S Bangera <Shrilaxmi S Bangera>
2022-02-18 19:52:07 +0530

    PracticeProj

---
+++ src/app/My_components/todos/todos.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos:Todo[];
+  constructor() {
+    this.todos=[{
+      sno:1,
+      title:"This is title1",
+      desc:"Description",
+      active:true
+    },
+    {
+      sno:2,
+      title:"This is title2",
+      desc:"Description",
+      active:true
+    }
+  ]
+  }
+
+  ngOnInit(): void {
+  }
+  deleteTodo(todo:Todo){
+    console.log(todo);
+    const index=this.todos.indexOf(todo);
+    this.todos.splice(index,1);
+  }
+  addTodo(todo:Todo){
+    console.log(todo);
+    this.todos.push(todo);
+  }
+}
